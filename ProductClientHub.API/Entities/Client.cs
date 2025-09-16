744c3817906aa051cbc9565c8e9a7e4db39886ea
Anderson Feltes <andermateus8@gmail.com>
2025-09-15 22:52:48 -0300

    Adicionado entidades para inserir os dados as tabelas do banco de dados e configurado o banco de dados do projeto

---
+++ ProductClientHub.API/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductClientHub.API.Entities;
+
+
+//essa entidade vai representar o cliente no sistema, no caso a linha no banco de dados na tabela de clientes
+public class Client
+    {
+        Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+}
+
